d55587d51b558b1a63af1690c38fb713314d86ed
Joseph_Thairu <joejoement@gmail.com>
2024-03-25 12:06:54 +0300

    completed

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback("Hello world")
+}function returnsANamedFunction(){
+        return function namedFunction(named){
+            }
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+    
+    }
+
+}
